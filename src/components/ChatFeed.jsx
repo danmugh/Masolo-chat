386d938955d2f714c3f859e933d5ac2f6268769d
Dan Mugisho <danmmwx@gmail.com>
2021-03-24 22:34:32 +0300

    Updating the ChatFeed

--- src/components/ChatFeed.jsx
+++ src/components/ChatFeed.jsx
@@ -3,21 +3,75 @@ import MyMessage from "./MyMessage";
 import TheirMessage from "./TheirMessage";
 
 const ChatFeed = (props) => {
-    // console.log(props)
+    console.log(props)
 
     const { chats, activeChat, userName, messages } = props;
 
     const chat = chats && chats[activeChat]
 
-    console.log('chats :', chats)
-    console.log('activeChat :', activeChat)
-    console.log('chat :', chat)
-    console.log('userName :', userName)
-    console.log('messages :', messages)
+    const renderReadReceipts = (message, isMyMessage) => {
+        return (
+            chat.people.map((person, index) => person.last_read === message.id && (
+                <div
+                    key={`read_${index}`}
+                    className="read-receipt"
+                    style={{
+                        float: isMyMessage ? 'right' : 'left',
+                        backgroundImage: person.person.avatar && `url(${person.person.avatar})`,
+                    }}
+                />
+            ))
+        )
+    }
+
+    // console.log('chats :', chats)
+    // console.log('activeChat :', activeChat)
+    // console.log('chat :', chat)
+    // console.log('userName :', userName)
+    // console.log('messages :', messages)
+
+    const renderMessages = () => {
+        const keys = Object.keys(messages);
+
+        return keys.map((key, index) => {
+            const message = messages[key];
+            const lastMessageKey = index === 0 ? null : keys[index - 1];
+            const isMyMessage = userName === message.sender.username;
+
+            return (
+                <div key={`msg_${index}`} style={{ width: '100%' }}>
+                    <div className="message-block">
+                        {
+                            isMyMessage
+                            ? <MyMessage message={message} />
+                            : <TheirMessage message={message} lastMessage={messages[lastMessageKey]} />
+                        }
+                    </div>
+                    <div className="read-receipts" style={{ marginRight: isMyMessage ? '18px' : '0px', marginLeft: isMyMessage ? '0px' : '68px' }}>
+                        {renderReadReceipts(message, isMyMessage)}
+                    </div>
+                </div>
+            );
+        });
+    };
+
+    renderMessages()
+
+    if (!chat) return 'Loading'
 
     return (
-        <div>
-            ChatFeed
+        <div className="chat-feed">
+            <div className="chat-title-container">
+                <div className="chat-title">{chat?.title}</div>
+                <div className="chat-subtitle">
+                    {chat.people.map((person) => `${person.person.username}`)}
+                </div>
+            </div>
+            {renderMessages()}
+            <div style={{ height: '100px' }} />
+            <div className="message-form-container">
+                <MessageForm {...props} chatId={activeChat} />
+            </div>
         </div>
     )
 }
--- src/components/MessageForm.jsx
+++ src/components/MessageForm.jsx
@@ -1,9 +1,112 @@
+import { useState } from 'react';
+import { SendOutlined, PictureOutlined } from '@ant-design/icons';
+import { sendMessage, isTyping } from 'react-chat-engine';
+// import { Input, Space } from 'antd';
+// import { PictureFilled,  } from '@ant-design/icons';
+
+// const { Search } = Input;
+
 const MessageForm = (props) => {
+    const [value, setValue] = useState('');
+    const { chatId, creds } = props;
+
+    const handleChange = (event) => {
+        setValue(event.target.value);
+
+        isTyping(props, chatId);
+    };
+
+    const handleSubmit = (event) => {
+        event.preventDefault();
+
+        const text = value.trim();
+
+        if (text.length > 0) {
+            sendMessage(creds, chatId, { text });
+        }
+
+        setValue('');
+    };
+
+    const handleUpload = (event) => {
+        sendMessage(creds, chatId, { files: event.target.files, text: '' });
+    };
+
+    // const suffix = (
+    //     <div>
+    //         <label htmlFor="upload-button">
+    //     <span
+    //         className="image-button"
+    //     >
+    //       <PictureFilled
+    //           className="picture-icon"
+    //           style={{
+    //               fontSize: 16,
+    //               color: '#1890ff',
+    //           }}
+    //       />
+    //     </span>
+    //         </label>
+    //         <input
+    //             type="file"
+    //             multiple={false}
+    //             id="upload-button"
+    //             style={{ display: 'none' }}
+    //             onChange={handleUpload.bind(this)}
+    //         />
+    //         <button
+    //             type="submit"
+    //             // className="send-button"
+    //         >
+    //             <SendOutlined className="send-icon" />
+    //         </button>
+    //     </div>
+    //
+    // );
+
     return (
-        <div>
-            MessageForm
-        </div>
-    )
-}
+        <form className="message-form" onSubmit={handleSubmit}>
+            <input
+                className="message-input"
+                placeholder="Send a message..."
+                value={value}
+                onChange={handleChange}
+                onSubmit={handleSubmit}
+            />
+            <label htmlFor="upload-button">
+            <span className="image-button">
+              <PictureOutlined className="picture-icon" />
+            </span>
+            </label>
+            <input
+                type="file"
+                multiple={false}
+                id="upload-button"
+                style={{ display: 'none' }}
+                onChange={handleUpload.bind(this)}
+            />
+            <button type="submit" className="send-button">
+                <SendOutlined className="send-icon" />
+            </button>
+
+            {/*<Input*/}
+            {/*    // className="message-input"*/}
+
+            {/*    placeholder="Type your message text"*/}
+            {/*    enterButton={handleSubmit}*/}
+            {/*    size="large"*/}
+            {/*    suffix={suffix}*/}
+            {/*    value={value}*/}
+            {/*    allowClear*/}
+            {/*    onChange={handleChange}*/}
+            {/*    onSubmit={handleSubmit}*/}
+            {/*    onSearch={handleSubmit}*/}
+            {/*/>*/}
+
+        </form>
+
+
+    );
+};
 
 export default MessageForm;
